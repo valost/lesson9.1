002496195e8e892aa72e169e2c92c8f6326f4e0c
valost <valerieostrovska@yahoo.com>
2023-05-22 17:21:42 +0300

    added functions

---
+++ app.js
@@ -0,0 +1,18 @@
+let modal = document.getElementById("my-modal");
+let btn = document.getElementById("mybtn");
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none"
+}
+
+window.onclick = function (event) {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+}
